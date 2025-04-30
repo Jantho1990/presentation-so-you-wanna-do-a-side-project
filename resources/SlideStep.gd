b4fe1656b15d7202fe14e4ecd0d46be497b1d91f
Josh Anthony <jantho1990@gmail.com>
2025-04-30 10:02:21 -0500

    added animations for HowLongDoesItTake; refactored slide to position steps to specify which axis to use; refactored slide to position steps to not require animation containers to work

--- resources/SlideStep.gd
+++ resources/SlideStep.gd
@@ -38,7 +38,7 @@ func _on_Step_previous_ended() -> void:
   
   
 # Runs when the slide moves to the next step.
-# Interface function. Must be replaced by inhereting resources.
+# Interface function. Must be replaced by inheriting resources.
 func next() -> void:
 #  step_next_began.emit()
 #  step_next_ended.emit()
@@ -46,7 +46,7 @@ func next() -> void:
   
   
 # Runs when the slide moves to the previous step.
-# Interface function. Must be replaced by inhereting resources.  
+# Interface function. Must be replaced by inheriting resources.  
 func previous() -> void:
 #  step_previous_began.emit()
 #  step_previous_ended.emit()
