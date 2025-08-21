1c43c2bdb407c149eb0a40f25257f6786d358d55
Josh Anthony <jantho1990@gmail.com>
2025-08-20 19:33:23 -0500

    add debouncing for next step logic

--- autoload/Presenter.gd
+++ autoload/Presenter.gd
@@ -1,12 +1,18 @@
 extends Node
 
 
+const DEBOUNCE_DURATION = 0.15
+
 @export var activePresentation : Presentation
+
+var _debouncing := false
+
+@onready var _debounceTimer = Timer.new()
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-  pass # Replace with function body.
+  _set_up_debounce_timer()
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -15,13 +21,15 @@ func _process(delta):
 
 
 func _input(_inputEvent: InputEvent) -> void:
-  if not activePresentation:
+  if not activePresentation or _debouncing:
     return
   
   if Input.is_action_just_pressed("slide_step_next"):
     activePresentation.next()
+    _debounce()
   elif Input.is_action_just_pressed("slide_step_prev"):
     activePresentation.previous()
+    _debounce()
   elif Input.is_action_just_pressed("slide_next"):
     pass
   elif Input.is_action_just_pressed("slide_prev"):
@@ -35,6 +43,21 @@ func _input(_inputEvent: InputEvent) -> void:
 func _on_ActivePresentation_finished() -> void:
   activePresentation.hide()
   activePresentation = null
+
+
+func _on_DebounceTimer_timeout() -> void:
+  _debouncing = false
+
+
+func _debounce() -> void:
+  _debouncing = true
+  _debounceTimer.start(DEBOUNCE_DURATION)
+
+
+func _set_up_debounce_timer() -> void:
+  _debounceTimer.one_shot = true
+  add_child(_debounceTimer)
+  _debounceTimer.timeout.connect(_on_DebounceTimer_timeout)
 
 
 # Makes the presentation ready for use.
